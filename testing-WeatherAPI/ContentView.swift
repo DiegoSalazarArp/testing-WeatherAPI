4267736af7f40937c3eb144b767abc93827b145c
Diego Salazar Arp <d.salazararp@gmail.com>
2019-08-20 19:44:00 -0400

    Initial Commit

---
+++ testing-WeatherAPI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  testing-WeatherAPI
+//
+//  Created by Diego Salazar Arp on 20-08-19.
+//  Copyright © 2019 Diego Salazar Arp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
